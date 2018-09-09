31ffe62f8d17efbf1d82dd3d2ff70332e8c0dcfd
Anzhela Poghosyan <angelpoghosyan@Angels-MacBook-Air.local>
2018-09-09 15:19:29 +0400

    first commit

---
+++ hmw.js
@@ -0,0 +1,11 @@
+const a=1;
+const b=21-3;
+const c=`hello`+` `+`world`;  
+
+if(a===b && b===18) {
+  console.log(`good job`)
+} else if(c===`hello world`) {
+  console.log(`maybe try again`)
+}
+
+alert('if this pops up then I did it')
